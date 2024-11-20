427833c493aeedecb3d309b1c904610dd768bc33
Audun Sektnan <sek@nr.no>
2024-11-20 13:22:10 +0100

    CCS-213: Fixes for plume groups.

--- webviz_subsurface/plugins/_co2_leakage/_utilities/co2volume.py
+++ webviz_subsurface/plugins/_co2_leakage/_utilities/co2volume.py
@@ -111,7 +111,7 @@ def _prepare_pattern_and_color_options(
     mark_choice: str,
 ) -> Tuple[Dict, List, List]:
     print("\n_prepare_pattern_and_color_options")
-    print(containment_info)  # NBNB-AS: Mangler plume_groups, har bare plume_group
+    print(containment_info)
     mark_options = [] if mark_choice == "none" else containment_info[f"{mark_choice}s"]
     color_options = containment_info[f"{color_choice}s"]
     num_colors = len(color_options)
@@ -529,9 +529,11 @@ def generate_co2_time_containment_figure(
     options = _prepare_line_type_and_color_options(
         df, containment_info, color_choice, mark_choice
     )
+    # NBNB-AS: Missing total for plume groups
     active_cols_at_startup = list(
         options[options["line_type"].isin(["solid", "0px"])]["name"]
     )
+    print(f"\nactive_cols_at_startup: {active_cols_at_startup}")
     fig = go.Figure()
     # Generate dummy scatters for legend entries
     dummy_args = {"x": df["date"], "mode": "lines", "hoverinfo": "none"}
