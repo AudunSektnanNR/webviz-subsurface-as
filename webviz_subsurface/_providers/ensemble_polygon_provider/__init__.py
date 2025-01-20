ceaffb41c9a89a256c17f220ff761903340722f1
Audun Sektnan <sek@nr.no>
2025-01-20 10:00:29 +0100

    isort

--- webviz_subsurface/_providers/ensemble_polygon_provider/__init__.py
+++ webviz_subsurface/_providers/ensemble_polygon_provider/__init__.py
@@ -1,8 +1,3 @@
-from .ensemble_polygon_provider import (
-    EnsemblePolygonProvider,
-    SimulatedPolygonsAddress,
-)
-from .ensemble_polygon_provider_factory import (
-    EnsemblePolygonProviderFactory,
-)
-from .polygon_server import PolygonsAddress, FaultPolygonsServer
+from .ensemble_polygon_provider import EnsemblePolygonProvider, SimulatedPolygonsAddress
+from .ensemble_polygon_provider_factory import EnsemblePolygonProviderFactory
+from .polygon_server import FaultPolygonsServer, PolygonsAddress
